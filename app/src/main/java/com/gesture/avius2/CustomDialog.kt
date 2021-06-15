f01e7ec654cf4428625e244c024023455629193f
mirwisek <mirwise001@gmail.com>
2021-06-15 20:19:15 +0500

    Binded fragment_start.xml with MainActivity.kt and CustomDialog.kt
    Former-commit-id: 67606ea5ed8ab6e00c393d5859a4ace3ef1751df [formerly 63d6d85709acf035516a1986b74c22826986b5ea]
    Former-commit-id: 1dce917722817f7e5a1d636f0f965463272aa134

---
+++ app/src/main/java/com/gesture/avius2/CustomDialog.kt
@@ -0,0 +1,37 @@
+package com.gesture.avius2
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import com.google.android.material.button.MaterialButton
+
+class CustomDialog(context: Context) : Dialog(context) {
+
+    private var onYes: (() -> Unit)? = null
+    private var onNo: (() -> Unit)? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog)
+
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        findViewById<MaterialButton>(R.id.btn_yes).setOnClickListener {
+            onYes?.invoke()
+        }
+        findViewById<MaterialButton>(R.id.btn_no).setOnClickListener {
+            onNo?.invoke()
+        }
+    }
+
+    fun setOnYesListener(onYes: () -> Unit) {
+        this.onYes = onYes
+    }
+
+    fun setOnNoListener(onNo: () -> Unit) {
+        this.onNo = onNo
+    }
+
+}
