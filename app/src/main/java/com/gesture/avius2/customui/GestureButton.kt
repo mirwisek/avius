30acab93b2fbc73767c8e82bedc3603f06b6e2b0
mirwise <mirwise001@gmail.com>
2021-06-30 19:52:57 +0500

    Added parameters to GestureButton.kt for more control and fixed sizes
    Former-commit-id: f33499a6035a6de465129bc00355084d5890774f [formerly 225ed06bd34a71ab3014157348cf957ede575872]
    Former-commit-id: 5cf3d15eff8b71ca60949440a8ba4b05d810060a

--- app/src/main/java/com/gesture/avius2/customui/GestureButton.kt
+++ app/src/main/java/com/gesture/avius2/customui/GestureButton.kt
@@ -1,23 +1,24 @@
 package com.gesture.avius2.customui
 
 import android.content.Context
-import android.graphics.BlendMode
 import android.graphics.drawable.GradientDrawable
-import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.LayerDrawable
+import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ProgressBar
-import androidx.core.graphics.BlendModeColorFilterCompat
-import androidx.core.graphics.BlendModeCompat
+import android.widget.RelativeLayout
+import androidx.annotation.RequiresApi
 import androidx.core.view.marginBottom
 import androidx.core.view.setPadding
 import com.gesture.avius2.R
-import com.google.android.material.circularreveal.CircularRevealRelativeLayout
 
 class GestureButton @JvmOverloads
 constructor(private val ctx: Context, private val attrs: AttributeSet? = null) :
-    CircularRevealRelativeLayout(ctx, attrs) {
+    RelativeLayout(ctx, attrs) {
 
     var progressBar: ProgressBar
     var circle: ImageView
@@ -26,12 +27,13 @@ constructor(private val ctx: Context, private val attrs: AttributeSet? = null) :
         val inflater = ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val root = inflater.inflate(R.layout.thumb_button, this)
 
+        val imgShadow = root.findViewById<ImageView>(R.id.imageShadow)
         val thumb = root.findViewById<ImageView>(R.id.thumb)
-        val container = root.findViewById<CircularRevealRelativeLayout>(R.id.container)
+        val container = root.findViewById<RelativeLayout>(R.id.container)
+        val content = root.findViewById<RelativeLayout>(R.id.content)
         circle = root.findViewById(R.id.circle)
 
         progressBar = root.findViewById(R.id.progress)
-        val progressMarginBottom = progressBar.marginBottom
 
         val res = ctx.obtainStyledAttributes(attrs, R.styleable.GestureButton, 0, 0)
         try {
@@ -43,24 +45,56 @@ constructor(private val ctx: Context, private val attrs: AttributeSet? = null) :
             val padding = res.getDimensionPixelSize(R.styleable.GestureButton_layoutPadding, 0)
             container.setPadding(padding, padding, padding, padding)
 
-            val marginCircle = res.getDimensionPixelSize(R.styleable.GestureButton_marginCircle, 0)
-            val circleParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-            circleParams.setMargins(marginCircle, marginCircle, marginCircle, marginCircle)
-            circle.layoutParams = circleParams
+            val marginContent = res.getDimensionPixelSize(R.styleable.GestureButton_marginContent, 0)
+            content.setMargin(marginContent)
 
-            val marginProgress =
-                res.getDimensionPixelSize(R.styleable.GestureButton_marginProgress, 0)
-            val progressParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-            progressParams.setMargins(
-                marginProgress,
-                marginProgress,
-                marginProgress,
-                marginProgress + progressMarginBottom
-            )
-            progressBar.layoutParams = progressParams
+            val marginShadow = res.getDimensionPixelSize(R.styleable.GestureButton_marginShadow, 0)
+            imgShadow.setMargin(marginShadow)
+
+            val marginCircle = res.getDimensionPixelSize(R.styleable.GestureButton_marginCircle, 0)
+            circle.setMargin(marginCircle)
+
+            val shadowRadius = res.getDimension(R.styleable.GestureButton_shadowRadius, 0F)
+            val d = imgShadow.drawable as GradientDrawable
+            d.gradientRadius = shadowRadius
+
+            val innerRadiusRatio =
+                res.getFloat(R.styleable.GestureButton_progressInnerRadiusRatio, 4F)
+            val thicknessRatio =
+                res.getFloat(R.styleable.GestureButton_progressThicknessRatio, 30F)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                updateProgressParams(thicknessRatio, innerRadiusRatio)
+                progressBar.invalidate()
+            }
+
         } finally {
             res.recycle()
         }
+    }
+
+    private fun View.setMargin(marginValue: Int) {
+        val params = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+        params.setMargins(marginValue, marginValue, marginValue, marginValue)
+        layoutParams = params
+    }
+
+    private fun ViewGroup.setMargin(marginValue: Int) {
+        val params = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+        params.setMargins(marginValue, marginValue, marginValue, marginValue)
+        layoutParams = params
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun updateProgressParams(thicknessRatio: Float, innerRadiusRatio: Float) {
+        val progressDrawable = progressBar.progressDrawable as LayerDrawable
+        val dProgress = progressDrawable.findDrawableByLayerId(android.R.id.progress) as GradientDrawable
+        val dSecondary = progressDrawable.findDrawableByLayerId(android.R.id.secondaryProgress) as GradientDrawable
+
+        dProgress.thicknessRatio = thicknessRatio
+        dProgress.innerRadiusRatio = innerRadiusRatio
+
+        dSecondary.thicknessRatio = thicknessRatio
+        dSecondary.innerRadiusRatio = innerRadiusRatio
     }
 
     fun changeCircleColor(color: Int) {
